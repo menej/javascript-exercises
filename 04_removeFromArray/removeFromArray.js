c84da78d5c32805468f4befb151a90b1de343e65
menej <68961385+menej@users.noreply.github.com>
2023-08-10 13:04:55 +0200

    Added solution for 03, 04, 05 and 06

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (numArray) {
+    let i = 0;
+    while (i < numArray.length) {
+        let currentNum = numArray[i];
+        let numberRemoved = false;
+        for (let argumentIndex = 1; argumentIndex < arguments.length; argumentIndex++) {
+            if (currentNum === arguments[argumentIndex]) {
+                numArray.splice(i, 1);
+                numberRemoved = true;
+                break;
+            }
+        }
+        if (!numberRemoved) {
+            i++;
+        }
 
+    }
+    return numArray;
 };
 
 // Do not edit below this line
