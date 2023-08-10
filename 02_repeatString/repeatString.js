41b8f44d45ba14b18f9ec8d5472bff194423ede5
menej <68961385+menej@users.noreply.github.com>
2023-08-10 11:53:55 +0200

    Added solution for 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string,num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let constructedString = "";
+    for (let i = 0; i < num; i++) {
+        constructedString += string;
+    }
+    return constructedString;
 };
 
 // Do not edit below this line
